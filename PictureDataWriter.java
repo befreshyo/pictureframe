8a75bfe2e0c817319e263c6ee2089f56893d5910
lala <90055383+befreshyo@users.noreply.github.com>
2021-12-08 18:06:14 -0600

    added comments

--- PictureDataWriter.java
+++ PictureDataWriter.java
@@ -9,7 +9,6 @@ public class PictureDataWriter {
      * string fname is the file name (I tried using File fname, but wasn't working)
      * returns the next function, writeToTextFile else returns false
      */
-
     public static boolean writeToFile(ArrayList<PictureData> pd, String fname){
          if (fname.endsWith(".txt")){
              return writeToTextFile(pd, fname);
@@ -24,7 +23,6 @@ public class PictureDataWriter {
      * string fname is the file name
      * returns true if it was was able to write data else returns false
      */
-
     public static boolean writeToTextFile (ArrayList<PictureData> pd, String fname){
     try {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fname)));
